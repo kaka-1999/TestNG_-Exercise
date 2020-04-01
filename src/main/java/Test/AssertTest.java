089372d00ea018e490fbac96e33685bedf0dc591
kaka-1999 <762445526@qq.com>
2020-04-01 11:45:22 +0800

    新增类

---
+++ src/main/java/Test/AssertTest.java
@@ -0,0 +1,23 @@
+package Test;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class AssertTest {
+    /**
+     * 一个方法中执行两个断言
+     * 1.不添加softAssert.assertAll();即使断言失败也不会报错，所有代码都执行；
+     * 2.添加softAssert.assertAll();所有代码都执行，但也会报错断言执行失败；
+     */
+    @Test
+    public void TestAssert001(){
+        SoftAssert softAssert=new SoftAssert();
+        softAssert.assertEquals(1,1);
+        System.out.println("--");
+        softAssert.assertEquals(1,2);
+        System.out.println("--");
+        softAssert.assertAll();
+
+    }
+
+}
---
+++ src/main/java/Test/TestCase002.java
@@ -0,0 +1,37 @@
+package Test;
+
+import org.testng.annotations.*;
+
+public class TestCase002 extends SuiteTest{
+
+    @BeforeClass
+    public void BeforeClassTest() {
+        System.out.println("-----TestCase002-----------BeforeClassTest------");
+    }
+
+    @BeforeMethod
+    public void BeforeMethodTest() {
+        System.out.println("-----TestCase002-----------BeforeMethodTest------");
+    }
+
+    @Test
+    public void TestMethod001() {
+        System.out.println("-----TestCase002-----------TestMethod001------");
+    }
+
+    @Test
+    public void TestMethod002() {
+        System.out.println("-----TestCase002-----------TestMethod002------");
+    }
+
+    @AfterMethod
+    public void AfterMethodTest() {
+        System.out.println("-----TestCase002-----------AfterMethodTest------");
+    }
+
+    @AfterClass
+    public void AfterClassTest() {
+        System.out.println("-----TestCase002-----------AfterClassTest------");
+    }
+
+}
